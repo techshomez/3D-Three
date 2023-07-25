929bf3cd1a7967ce36b1b9a6b23c609b524a7c4d
techshomez <danieltopeg@gmail.com>
2023-07-25 16:08:50 +0100

    Home page component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,13 @@ import Customizer from "./pages/customizer";
 import Home from "./pages/home";
 
 function App() {
-  return <div className="head-text">Three.js</div>;
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
+  );
 }
 
 export default App;
