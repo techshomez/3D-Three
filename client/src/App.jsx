4e7c3bdde14e0d3ad1e1ba92d0643f26164f1259
techshomez <danieltopeg@gmail.com>
2023-07-25 15:59:48 +0100

    working on pages and folder structures

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,7 @@
+import Canvas from "./cancas";
+import Customizer from "./pages/customizer";
+import Home from "./pages/home";
+
 function App() {
   return <div className="head-text">Three.js</div>;
 }
